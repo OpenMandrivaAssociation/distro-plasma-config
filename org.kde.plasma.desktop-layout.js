5d4bbc96a784d135d7e6f580140c542500331196
Tomasz Paweł Gajc <tpgxyz@gmail.com>
2015-10-10 16:50:12 +0400

    remove it

--- org.kde.plasma.desktop-layout.js
+++ org.kde.plasma.desktop-layout.js
@@ -8,7 +8,6 @@ loadTemplate("org.openmandriva.plasma.desktop.defaultPanel");
 for (var i = 0; i < screenCount; ++i) {
     var id = createActivity("Desktop", "org.kde.plasma.folder");
     var desktopsArray = desktopsForActivity(id);
-    print(desktopsArray.length);
     for( var j = 0; j < desktopsArray.length; j++) {
         desktopsArray[j].wallpaperPlugin = 'org.kde.image';
         desktopsArray[j].currentConfigGroup = ["Wallpaper", desktopsArray[j].wallpaperPlugin, "General"];
