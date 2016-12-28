f6deb6ed8557bebddc9f5d9025b71047b0fa7cf8
tpg <tpgxyz@gmail.com>
2016-12-28 19:25:01 +0100

    adjust JS to latest Plasma changes

--- org.kde.plasma.desktop-layout.js
+++ org.kde.plasma.desktop-layout.js
@@ -5,32 +5,20 @@
 print("Starting OpenMandriva Plasma configuration")
 loadTemplate("org.openmandriva.plasma.desktop.defaultPanel")
 
-for (var i = 0; i < screenCount; ++i) {
-    var id = createActivity("Desktop", "org.kde.plasma.folder")
-    var desktopsArray = desktopsForActivity(id);
-    for( var j = 0; j < desktopsArray.length; j++) {
-        desktopsArray[j].wallpaperPlugin = 'org.kde.image'
-        desktopsArray[j].wallpaperMode = 'SingleImage'
-        desktopsArray[j].currentConfigGroup = new Array("General")
-        desktopsArray[j].writeConfig("pressToMove",true)
-        desktopsArray[j].writeConfig("showToolbox",false)
-        desktopsArray[j].writeConfig("toolTips", "true")
-        desktopsArray[j].writeConfig("selectionMarkers",false)
-        desktopsArray[j].writeConfig("sortMode","-1")
-        desktopsArray[j].currentConfigGroup = new Array("Wallpaper", "org.kde.image", "General")
-        desktopsArray[j].writeConfig("Image", "file:///usr/share/mdk/backgrounds/default.png")
-        desktopsArray[j].writeConfig("FillMode","2")
-    }
-
-// Create more panels for other screens
-    if (i > 0){
-        var panel = new Panel
-        panel.screen = i
-        panel.location = 'bottom';
-        panel.height = panels()[i].height = screenGeometry(0).height > 1024 ? 35 : 27;
-        var tasks = panel.addWidget("tasks")
-        tasks.writeConfig("showOnlyCurrentScreen", true)
-    }
+var desktopsArray = desktopsForActivity(currentActivity());
+for( var j = 0; j < desktopsArray.length; j++) {
+    desktopsArray[j].wallpaperPlugin = 'org.kde.image';
+    desktopsArray[j].wallpaperMode = 'SingleImage';
+    desktopsArray[j].currentConfigGroup = new Array("General")
+    desktopsArray[j].writeConfig("pressToMove",true)
+    desktopsArray[j].writeConfig("showToolbox",false)
+    desktopsArray[j].writeConfig("toolTips", true)
+    desktopsArray[j].writeConfig("selectionMarkers",false)
+    desktopsArray[j].writeConfig("arrangement","1")
+    desktopsArray[j].writeConfig("iconSize","4")
+    desktopsArray[j].currentConfigGroup = new Array("Wallpaper", "org.kde.image", "General")
+    desktopsArray[j].writeConfig("Image", "file:///usr/share/mdk/backgrounds/default.png")
+    desktopsArray[j].writeConfig("FillMode","2")
 }
 
 sleep(0.5)
